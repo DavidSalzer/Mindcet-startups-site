575293fe8a22dcfbd413667196c50432d9772cf3
yanai <edriyanai@gmail.com>
2014-02-06 13:22:26 +0200

    capch

--- wp-content/themes/mindcet-Theme/functions.php
+++ wp-content/themes/mindcet-Theme/functions.php
@@ -715,3 +715,36 @@ function mailChimp($email,$name){
                                        );
     if($result)return true;
 	}
+
+
+
+function myCapch(){
+	require( get_template_directory() . 'inc/recaptcha-php/recaptchalib.php' );
+	// Get a key from https://www.google.com/recaptcha/admin/create
+	$publickey = "6Lc_Pu4SAAAAAPo3yZJ8UQkagt5Wm_tA4W5x8Qpz";
+	$privatekey = "6Lc_Pu4SAAAAAP4_SfbPOk9VHWyJnFhU-4HPSgX1";
+	
+	# the response from reCAPTCHA
+	$resp = null;
+	# the error code from reCAPTCHA, if any
+	$error = null;
+	
+	# was there a reCAPTCHA response?
+	if ($_POST["recaptcha_response_field"]) {
+			$resp = recaptcha_check_answer ($privatekey,
+											$_SERVER["REMOTE_ADDR"],
+											$_POST["recaptcha_challenge_field"],
+											$_POST["recaptcha_response_field"]);
+	
+			if ($resp->is_valid) {
+					echo "You got it!";
+			} else {
+					# set the error code so that we can display it
+					$error = $resp->error;
+			}
+	}
+	echo recaptcha_get_html($publickey, $error);
+	
+
+
+}

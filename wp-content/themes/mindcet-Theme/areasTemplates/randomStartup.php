26c6dcdfeb962e8ba217b3b3f056f40ee84a0af6
yishainstern <syishai@cambium.co.il>
2015-04-15 18:27:13 +0300

    יחיד

--- wp-content/themes/mindcet-Theme/areasTemplates/randomStartup.php
+++ wp-content/themes/mindcet-Theme/areasTemplates/randomStartup.php
@@ -3,7 +3,7 @@
     <div class="middelBanner">
         <h2> The Startup</h2>
     </div>
-    <div class="page-wrap ">
+    <div class="page-wrap rand-page-wrap">
     <?php 
         $args = array(
             'showposts'   => 1,
@@ -16,11 +16,14 @@
         foreach ( $myposts as $post ) : setup_postdata( $post );
         
     ?>
-        <div><?php echo get_the_title($post->ID);?></div>
-        <div><?php echo apply_filters ("the_content", $post->post_content);?></div>
-        <div class="img-wrap">
+        <div class="img-wrap img-rand rand-box">
             <?php echo get_the_post_thumbnail( $post->ID,array(220,155), $attr ); ?>
         </div>
+        <div class="rand-wrapper-content rand-box">
+            <div class="rand-title"><?php echo get_the_title($post->ID);?></div>
+            <div class="rand-content"><?php echo apply_filters ("the_content", $post->post_content);?></div>
+        </div>
+        
         <?php   endforeach; ?>
     </div>
 </div>
